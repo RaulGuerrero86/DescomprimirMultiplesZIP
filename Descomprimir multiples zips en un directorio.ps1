b2a62c0bc9fff08a2ecfb112c680e7102a6062c5
Raul Guerrero <53845611+RaulGuerrero86@users.noreply.github.com>
2019-12-16 11:38:25 -0600

    Script PowerShell
    Este script te ayudará a descomprimir "n" cantidad de archivos ZIP en un solo directorio!

---
+++ Descomprimir multiples zips en un directorio.ps1
@@ -0,0 +1,25 @@
+﻿PARAM (
+    
+    [string] $ZipFilesPath = "C:\Archivos recibidos",
+    [string] $ZipFilesDest = "C:\Archivos descomprimidos"
+
+)
+
+$Shell = New-Object -com Shell.Application
+$Location = $Shell.NameSpace($ZipFilesDest)
+
+$ZipFiles = Get-ChildItem $ZipFilesPath -Recurse -Include *.ZIP
+
+# Aquí iniciamos el Bucle para reccorrer los archivos ZIP y descomprimirlos en la carpeta Dest (Destino)
+
+$i
+
+foreach ($ZipFile in $ZipFiles) {
+    
+    Write-Progress -Activity "Descromprimiendo en $($ZipFilesDest)" -PercentComplete (($i / ($ZipFiles.count +1))*100) -CurrentOperation $ZipFile.FullName -Status " Archivo $($ZipFiles.count)"
+
+    $ZipFolder = $Shell.NameSpace($ZipFile.fullname)
+
+    $Location.Copyhere($ZipFolder.items(), 1040)
+    $i++
+}
